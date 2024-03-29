d6cbd8c36af6e6a61f2598deb2ad3b686d4a5f1f
Samanta DA SILVA <developpement.sam@gmail.com>
2024-03-29 17:00:43 +0100

    info Cards done, need to work on the stars rating system

--- src/components/InfoCard/infoCard.jsx
+++ src/components/InfoCard/infoCard.jsx
@@ -1,10 +1,34 @@
+import Tag from "../Tag/tag";
 import style from "./infoCard.module.scss";
+import { accommodationsData } from "../../assets/data/logements";
+import { useParams } from "react-router-dom";
 
 const InfoCard = ({ title, location }) => {
+  const { id } = useParams();
+  const accommodation = accommodationsData.find((item) => item.id === id);
+  const tagsList = accommodation.tags;
+  const hostName = accommodation.host.name;
+  const picture = accommodation.host.picture;
+
   return (
     <div className={style.container}>
-      <h1 className={style.title}>{title}</h1>
-      <p>{location}</p>
+      <div className={style.mainInfo}>
+        <h1 className={style.title}>{title}</h1>
+        <p className={style.location}>{location}</p>
+        <ul className={style.tagContainer}>
+          {tagsList.map((tag, index) => (
+            <Tag key={index} tagValue={tag} />
+          ))}
+        </ul>
+      </div>
+      <div className={style.hostInfo}>
+        <div className={style.name}>{hostName}</div>
+        <div
+          className={style.hostPicture}
+          style={{ backgroundImage: `url(${picture})` }}
+        ></div>
+        <div className={style.rating}></div>
+      </div>
     </div>
   );
 };
